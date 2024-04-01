b5af27b42f1e24dfcac69774d8e3ab18a7a6b1cb
MinJoon_Choi <minjoon1998@gmail.com>
2024-04-02 02:25:35 +0900

    feat: shared 화면 구현

---
+++ pages/shared.tsx
@@ -0,0 +1,61 @@
+import FolderInfo from '@/components/FolderInfo';
+import SearchBar from '@/components/SearchBar';
+import LinkList from '@/components/LinkList';
+import { useCallback, useEffect, useState } from 'react';
+import { getFolder } from '@/services/api';
+import style from '@/styles/Main.module.css';
+import useAsync from '@/hooks/useAsync';
+
+interface FolderInfoProp {
+  name: string;
+  owner?: {
+    name: string;
+    profileImageSource: string;
+  };
+}
+
+interface LinkShared {
+  id: number;
+  title: string;
+  url: string;
+  createdAt: string;
+  created_at: string;
+  imageSource: string;
+  image_source: string;
+}
+
+export default function Shared() {
+  const [linkList, setLinkList] = useState<LinkShared[]>([]);
+  const [folderInfo, setFolderInfo] = useState<FolderInfoProp>({
+    name: '',
+    owner: {
+      name: '',
+      profileImageSource: '',
+    },
+  });
+  const asyncGetFolder = useAsync(getFolder);
+
+  const apiGetFolder = useCallback(async () => {
+    const result = await asyncGetFolder();
+    if (!result) return;
+
+    const { folder } = result;
+    const { links, name, owner } = folder;
+    setFolderInfo({ name, owner });
+    setLinkList([...links]);
+  }, [asyncGetFolder]);
+
+  useEffect(() => {
+    apiGetFolder();
+  }, [apiGetFolder]);
+
+  return (
+    <main className={style.main}>
+      <FolderInfo folderInfo={folderInfo} />
+      <div className={style.mainContent}>
+        <SearchBar />
+        <LinkList linkList={linkList} />
+      </div>
+    </main>
+  );
+}

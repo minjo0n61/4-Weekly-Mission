d537b397e9de408e8583cb3e3c0f8fde8b5ce9be
MinJoon_Choi <minjoon1998@gmail.com>
2024-04-02 01:59:32 +0900

    feat: _app.tsx, _document.tsx 파일 설정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,36 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ko">
+      <Head>
+        <meta charSet="utf-8" />
+        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content="Web site created using create-react-app"
+        />
+        <meta property="og:title" content="Linkbrary" />
+        <meta property="og:url" content="" />
+        <meta property="og:image" content="/logo.png" />
+        <meta
+          property="og:description"
+          content="세상의 모든 정보를 쉽게 저장하고 관리해보세요"
+        />
+        <script
+          src="https://t1.kakaocdn.net/kakao_js_sdk/2.6.0/kakao.min.js"
+          integrity="sha384-6MFdIr0zOira1CHQkedUqJVql0YtcZA1P0nbPrQYJXVJZUkTk/oX4U9GhUIs3/z8"
+          crossOrigin="anonymous"
+        ></script>
+        <title>Linkbrary</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <div id="modal-root"></div>
       </body>
     </Html>
-  )
+  );
 }

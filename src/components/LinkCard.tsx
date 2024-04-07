c31159982f3ee423a3046c76c571d1880f82c948
MinJoon_Choi <minjoon1998@gmail.com>
2024-04-07 19:10:26 +0900

    fix: 이미지 경로 오류로 안뜨는 이미지 기본값 설정

--- src/components/LinkCard.tsx
+++ src/components/LinkCard.tsx
@@ -23,16 +23,18 @@ interface LinkCard {
 
 function LinkCard({ data, openModal }: LinkCard) {
   const [popOver, setPopOver] = useState(false);
+  const [imgSrc, setImgSrc] = useState(
+    data?.imageSource || data?.image_source || noImg,
+  );
 
   const handleKebabClick = (e: MouseEvent<HTMLDivElement>) => {
     e.stopPropagation();
     setPopOver((prevData) => !prevData);
   };
 
-  // const noHttpProtocol = ((data: DataProp) => {
-  //   let src = data?.imageSource || data?.image_source;
-  //   data.imageSource = src.startsWith('//') ? `https:${src}` : src;
-  // })(data);
+  const handleError = () => {
+    setImgSrc(noImg);
+  };
 
   useEffect(() => {
     setPopOver(false);
@@ -46,59 +48,52 @@ function LinkCard({ data, openModal }: LinkCard) {
     };
   }, []);
   return (
-    <>
-      <div className={style.linkGridItem}>
-        <div className={style.itemImg}>
-          <Image
-            width={150}
-            height={150}
-            src={
-              data?.imageSource || data?.image_source
-                ? data?.imageSource || data?.image_source
-                : noImg
-            }
-            alt="no-img"
-          />
-        </div>
-        <div className={style.itemInfo}>
-          <div className={style.flexBox}>
-            <div className={style.dateDiff}>
-              {diffDate(dateParse(data?.createdAt || data?.created_at))}
-            </div>
-            <div className={style.kebabBox} onClick={handleKebabClick}>
-              <Image width={20} height={20} src={kebabImg} alt="kebab" />
-              {popOver && (
-                <div className={style.popOver}>
-                  <ul>
-                    <li onClick={() => openModal('LinkDelete', data)}>
-                      <div>삭제하기</div>
-                    </li>
-                    <li onClick={() => openModal('LinkAdd')}>
-                      <div>폴더에 추가</div>
-                    </li>
-                  </ul>
-                </div>
-              )}
-            </div>
-          </div>
-          <Link href={data?.url} target="_blank" rel="noreferrer">
-            <div className={style.itemContext}>{data?.title}</div>
-          </Link>
-          <div className={style.itemDate}>
-            {dateParse(data?.createdAt || data?.created_at)}
-          </div>
-        </div>
-
-        {/* svg조작으로 색상변경할 예정 */}
+    <div className={style.linkGridItem}>
+      <div className={style.itemImg}>
         <Image
-          width={30}
-          height={30}
-          className={style.starBtn}
-          src={starDefault}
-          alt="star"
+          width={150}
+          height={150}
+          src={imgSrc}
+          alt="link image"
+          onError={handleError}
         />
       </div>
-    </>
+      <div className={style.itemInfo}>
+        <div className={style.flexBox}>
+          <div className={style.dateDiff}>
+            {diffDate(dateParse(data?.createdAt || data?.created_at))}
+          </div>
+          <div className={style.kebabBox} onClick={handleKebabClick}>
+            <Image width={20} height={20} src={kebabImg} alt="kebab" />
+            {popOver && (
+              <div className={style.popOver}>
+                <ul>
+                  <li onClick={() => openModal('LinkDelete', data)}>
+                    <div>삭제하기</div>
+                  </li>
+                  <li onClick={() => openModal('LinkAdd')}>
+                    <div>폴더에 추가</div>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+        </div>
+        <Link href={data?.url} target="_blank" rel="noopener noreferrer">
+          <div className={style.itemContext}>{data?.title}</div>
+        </Link>
+        <div className={style.itemDate}>
+          {dateParse(data?.createdAt || data?.created_at)}
+        </div>
+      </div>
+      <Image
+        width={30}
+        height={30}
+        className={style.starBtn}
+        src={starDefault}
+        alt="star"
+      />
+    </div>
   );
 }
 

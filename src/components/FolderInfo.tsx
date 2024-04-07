6e5b74a60d11061db390d8be68b6fece7438b376
MinJoon_Choi <minjoon1998@gmail.com>
2024-04-08 08:27:29 +0900

    refactor: interface depth 줄이기, jsdoc 추가

--- src/components/FolderInfo.tsx
+++ src/components/FolderInfo.tsx
@@ -1,27 +1,37 @@
 import style from '@/styles/FolderInfo.module.css';
 
 interface FolderInfoProp {
-  folderInfo: {
+  name: string;
+  owner?: {
     name: string;
-    owner?: {
-      name: string;
-      profileImageSource: string;
-    };
+    profileImageSource: string;
   };
 }
 
-function FolderInfo({ folderInfo }: FolderInfoProp) {
+/**
+ * 사용자 폴더의 정보를 나타내는 컴포넌트
+ * 폴더의 name, owner, profileImage를 나타냄
+ *
+ * @component
+ * @param {FolderInfoProp} props - props에 적용되는 interface 타입
+ * @param {string} props.name - 폴더 name
+ * @param {Object} [props.owner] - 폴더 owner 정보
+ * @param {string} props.owner.name - 폴더 owner name
+ * @param {string} props.owner.profileImageSource - 폴더 owner profile image
+ */
+
+function FolderInfo({ name, owner }: FolderInfoProp) {
   return (
     <div id={style.folderInfo}>
       <div className={style.infoBox}>
         <div className={style.profileImg}>
-          <img src={folderInfo.owner?.profileImageSource} alt="profile" />
+          <img src={owner?.profileImageSource} alt="profile" />
         </div>
         <div className={style.userName}>
-          <div>{folderInfo.owner?.name}</div>
+          <div>{owner?.name}</div>
         </div>
         <div className={style.folderName}>
-          <div>{folderInfo?.name}</div>
+          <div>{name}</div>
         </div>
       </div>
     </div>

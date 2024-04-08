4d055d344f9237b2e214f34b3de7dbb962dc7d06
MinJoon_Choi <minjoon1998@gmail.com>
2024-04-08 18:18:55 +0900

    feat: signin, signup header, footer 예외처리

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,17 @@ import type { AppProps } from 'next/app';
 import '@/styles/App.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const exceptUI = ['/signin', '/signup'];
+  const showUI = !exceptUI.includes(router.pathname);
   return (
     <>
-      <Header />
+      {showUI && <Header />}
       <Component {...pageProps} />
-      <Footer />
+      {showUI && <Footer />}
     </>
   );
 }
